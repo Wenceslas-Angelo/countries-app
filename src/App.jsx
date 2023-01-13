aaa0302bf790c7f804ef26f3f7388930c0ad62e0
Wenceslas-Angelo <wenceslasangelo@gmail.com>
2023-01-13 20:25:39 +0300

    pagination

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { FaMoon } from 'react-icons/fa';
 import Todo from './components/Country';
+import ReactPaginate from 'react-paginate';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const countryPerPage = 8;
+  const pageVisited = pageNumber * countryPerPage;
 
   const getAllCountries = async () => {
     const response = await fetch(`https://restcountries.com/v3.1/all`);
@@ -26,17 +31,30 @@ function App() {
       </header>
 
       <div className="countries">
-        {countries.map((country) => (
-          <Todo
-            key={country.name.common}
-            image={country.flags.png}
-            region={country.region}
-            name={country.name.common}
-            population={country.population}
-            capital={country.capital}
-          />
-        ))}
+        {countries
+          .slice(pageVisited, pageVisited + countryPerPage)
+          .map((country) => (
+            <Todo
+              key={country.name.common}
+              image={country.flags.png}
+              region={country.region}
+              name={country.name.common}
+              population={country.population}
+              capital={country.capital}
+            />
+          ))}
       </div>
+      <ReactPaginate
+        previousLabel={'Previous'}
+        nextLabel={'Next'}
+        pageCount={Math.ceil(countries.length / countryPerPage)}
+        onPageChange={({ selected }) => setPageNumber(selected)}
+        containerClassName={'pagination-btns'}
+        previousLinkClassName={'previous-btn'}
+        nextLinkClassName={'next-btn'}
+        disabledClassName={'pagination-disabled'}
+        activeClassName={'pagination-active'}
+      />
     </div>
   );
 }
