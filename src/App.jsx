82622c99d65d4d30c1c41c8298686611daec8140
Wenceslas-Angelo <wenceslasangelo@gmail.com>
2023-01-16 09:38:05 +0300

    light/dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
 import { FaMoon } from 'react-icons/fa';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Details from './pages/Details';
 
-function App() {
-  return (
-    <div className="app">
-      <header>
-        <h1>Where in the world?</h1>
-        <div className="switch-theme">
-          <FaMoon /> Dark mode
-        </div>
-      </header>
+export const ThemeContext = createContext(null);
 
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/details/:name" element={<Details />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((theme) => (theme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="app" id={theme}>
+        <header>
+          <h1>Where in the world?</h1>
+          <div className="switch-theme" onClick={() => toggleTheme()}>
+            <FaMoon /> Dark mode
+          </div>
+        </header>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/details/:name" element={<Details />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
